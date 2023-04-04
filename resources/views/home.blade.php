91f542ca7c397f8ae15cb12dbab3e139c71dccb6
kelvinwahyudi <gokelvin4@gmail.com>
2023-04-04 22:39:00 +0700

    puterrrr
    footerr

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -96,6 +96,18 @@
      </section>
 
 
+    <footer>
+      <div class="links">
+        <a href="#home">Home</a>
+        <a href="#menu">Produk</a>
+        <a href="#about">Tentang Kami</a>
+      </div>
+
+      <div class="credit">
+        <p>Created by <span>CV LAWANG AGUNG SUKSES</span> | &copy; 2023</p>
+      </div>
+    </footer>
+
      
 
 

1610c25c5b0c8e767e18645aaea59c384bd7043e
kelvinwahyudi <gokelvin4@gmail.com>
2023-05-02 13:03:16 +0700

    Update footer.blade.php

--- resources/views/footer/footer.blade.php
+++ resources/views/footer/footer.blade.php
@@ -1,33 +1,48 @@
-<footer>
-    <div class="footer-container">
-      <div class="footer-logo">
-        <img src="logo.png" alt="Logo Perusahaan">
-      </div>
-      <div class="footer-menu">
-        <ul>
-          <li><a href="#">Beranda</a></li>
-          <li><a href="#">Produk</a></li>
-          <li><a href="#">Tentang Kami</a></li>
-          <li><a href="#">Kontak</a></li>
-        </ul>
-      </div>
-      <div class="footer-contact">
-        <p>Jl. Jend. Sudirman No. 123</p>
-        <p>Jakarta 12345, Indonesia</p>
-        <p>+62 123 456 7890</p>
-        <p>info@perusahaan.com</p>
-      </div>
-      <div class="footer-social-media">
-        <ul>
-          <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-          <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-          <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-          <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
-        </ul>
-      </div>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    @include('partials.navbar');
+
+    <div class="container">
+        @yield('container')
     </div>
-    <div class="footer-credits">
-      <p>&copy; 2023 Nama Perusahaan. Hak cipta dilindungi.</p>
-    </div>
-  </footer>
-  
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+ 
+    <script src="https://kit.fontawesome.com/3996a8b58a.js" crossorigin="anonymous"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+    @yield('scripts')
+    <footer class="mt-5 mb-5">
+      <hr/>
+      <div class="container">
+          <div class="row">
+              <div class="col text-center">
+                  &copy; CV LAWANG AGUNG SUKSES 2023
+              </div>
+          </div>
+      </div>
+</footer>
+</body>
+
+</html>
